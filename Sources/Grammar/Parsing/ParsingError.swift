0e3bf28dcbbe24cd52d2a5f1307a7d9d2002c2d4
taylorswift <kelvin13ma@gmail.com>
2023-09-27 04:00:21 +0000

    clean up doc warnings

--- Sources/Grammar/Parsing/ParsingError.swift
+++ Sources/Grammar/Parsing/ParsingError.swift
@@ -1,98 +1,98 @@
 import TraceableErrors
 
-/// An error type that indicates the furthest-successful parse, and provides a 
+/// An error type that indicates the furthest-successful parse, and provides a
 /// stack trace detailing how the parser got there.
-/// 
-/// None of the structured parsers defined in ``/swift-grammar`` throw this error 
+///
+/// None of the structured parsers defined in swift-grammar throw this error
 /// directly. Instead, the ``DefaultDiagnostics`` engine computes it
-/// based on the diagnostic engine’s internal state when encountering 
+/// based on the diagnostic engine’s internal state when encountering
 /// invalid input.
-/// 
+///
 /// Custom ``ParsingRule`` implementations should not throw this error either.
-@frozen public 
-struct ParsingError<Index>:TraceableError, CustomStringConvertible 
+@frozen public
+struct ParsingError<Index>:TraceableError, CustomStringConvertible
 {
     /// The underlying parsing error.
-    public 
+    public
     let underlying:Error
-    /// The index of the first invalid terminal in the input, or the ``Collection/.endIndex``
-    /// of the input. 
-    /// 
-    /// If the parser attempted to apply multiple rules via backtracking, this 
+    /// The index of the first invalid terminal in the input, or the ``Collection endIndex``
+    /// of the input.
+    ///
+    /// If the parser attempted to apply multiple rules via backtracking, this
     /// index comes from the longest successful parse.
-    public 
+    public
     let index:Index
-    public 
+    public
     let trace:[Frame]
-    
-    @inlinable public 
+
+    @inlinable public
     init(at index:Index, because error:Error, trace:[Frame])
     {
         self.underlying = error
-        self.index      = index 
+        self.index      = index
         self.trace      = trace
     }
 
-    public 
-    var notes:[String] 
+    public
+    var notes:[String]
     {
-        trace.map 
+        trace.map
         {
             if $0.construction is Void.Type
             {
                 return "while matching pattern '\($0.rule)'"
             }
-            else 
+            else
             {
                 return "while parsing value of type '\($0.construction)' by rule '\($0.rule)'"
             }
         }
     }
-    
-    static 
-    func annotate<Background>(_ range:Range<Index>, background:Background, 
-        renderer render:(Background.SubSequence) -> String, 
-        newline predicate:(Background.Element) -> Bool) 
-        -> String 
+
+    static
+    func annotate<Background>(_ range:Range<Index>, background:Background,
+        renderer render:(Background.SubSequence) -> String,
+        newline predicate:(Background.Element) -> Bool)
+        -> String
         where Background:BidirectionalCollection, Background.Index == Index
     {
-        // `..<` means this will print the previous line if the problematic 
+        // `..<` means this will print the previous line if the problematic
         // index references the newline itself
-        let beginning:String, 
+        let beginning:String,
             middle:String
         if let start:Index  = background[..<range.lowerBound].lastIndex (where: predicate)
         {
-            // can only remove newline if there is actually a preceeding newline 
+            // can only remove newline if there is actually a preceeding newline
             beginning = render(background[start..<range.lowerBound].dropFirst())
         }
-        else 
+        else
         {
             beginning = render(background[..<range.lowerBound])
-        } 
+        }
         let line:String
-        let   end:Index     = background[range.lowerBound...].firstIndex(where: predicate) ?? 
+        let   end:Index     = background[range.lowerBound...].firstIndex(where: predicate) ??
             background.endIndex
-        if range.upperBound < end 
+        if range.upperBound < end
         {
             middle  = render(background[range])
             line    = beginning + middle + render(background[range.upperBound..<end])
         }
-        else 
+        else
         {
             middle  = render(background[range.lowerBound..<end])
-            line    = beginning + middle 
+            line    = beginning + middle
         }
-        return 
+        return
             """
             \(line)
             \(String.init(repeating: " ", count: beginning.count))\
             ^\(String.init(repeating: "~", count: middle.count).dropLast())
             """
     }
-    public 
-    func annotate<Background>(source background:Background, 
+    public
+    func annotate<Background>(source background:Background,
         renderer:(Background.SubSequence) -> String,
-        newline:(Background.Element) -> Bool) -> String 
+        newline:(Background.Element) -> Bool) -> String
         where Background:BidirectionalCollection, Background.Index == Index
     {
         var annotation:String = self.underlying.headline()
