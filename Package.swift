642d5957896f06b03e35c48fc439488367d3fd21
taylorswift <kelvin13ma@gmail.com>
2024-04-22 21:21:18 +0000

    rename Testing module to Testing_ to deconflict with swift-testing

--- Package.swift
+++ Package.swift
@@ -4,28 +4,26 @@ import PackageDescription
 let package:Package = .init(
     name: "Swift Grammar",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
-    products:
-    [
+    products: [
         .library(name: "Grammar", targets: ["Grammar"]),
-        .library(name: "Testing", targets: ["Testing"]),
+        .library(name: "Testing_", targets: ["Testing_"]),
         .library(name: "TraceableErrors", targets: ["TraceableErrors"]),
     ],
-    dependencies:
-    [
+    dependencies: [
         //  This dependency only used for the Testing module’s concurrency support.
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.2.0"),
     ],
-    targets:
-    [
-        .target(name: "Grammar", dependencies:
-            [
+    targets: [
+        .target(name: "Grammar",
+            dependencies: [
                 .target(name: "TraceableErrors"),
             ]),
 
-        .target(name: "Testing", dependencies:
-            [
+        .target(name: "Testing_",
+            dependencies: [
                 .product(name: "Atomics", package: "swift-atomics"),
-            ]),
+            ],
+            path: "Sources/Testing"),
 
         .target(name: "TraceableErrors"),
     ]
