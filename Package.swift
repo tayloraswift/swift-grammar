0dac977b50bf677b2c3adabd7d5586a7b6e09b17
Dianna Ma <mdianna1989@gmail.com>
2025-01-15 22:29:43 +0000

    remove the obsolete Testing_ module, and its transitive swift-atomics dependency

--- Package.swift
+++ Package.swift
@@ -6,24 +6,13 @@ let package:Package = .init(
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(name: "Grammar", targets: ["Grammar"]),
-        .library(name: "Testing_", targets: ["Testing_"]),
         .library(name: "TraceableErrors", targets: ["TraceableErrors"]),
-    ],
-    dependencies: [
-        //  This dependency only used for the Testing module’s concurrency support.
-        .package(url: "https://github.com/apple/swift-atomics.git", from: "1.2.0"),
     ],
     targets: [
         .target(name: "Grammar",
             dependencies: [
                 .target(name: "TraceableErrors"),
             ]),
-
-        .target(name: "Testing_",
-            dependencies: [
-                .product(name: "Atomics", package: "swift-atomics"),
-            ],
-            path: "Sources/Testing"),
 
         .target(name: "TraceableErrors"),
     ]
